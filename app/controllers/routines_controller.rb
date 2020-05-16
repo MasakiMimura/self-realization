230cff9464fd9df2c56434fa38280ed0613c11b3
MasakiMimura <mimu36projectd@gmail.com>
2020-05-17 02:37:46 +0900

    markup-fix

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,11 +1,13 @@
 class RoutinesController < ApplicationController
   def create
-    Routine.create(routine_params)
+    for num in 1..routine_params[:turn].to_i do
+      @routine = Routine.create(routine_params)
+    end
     redirect_to root_path
   end
 
   private
   def routine_params
-    params.require(:routine).permit(:content).merge(user_id: current_user.id)
+    params.require(:routine).permit(:content, :turn).merge(user_id: current_user.id)
   end
 end
