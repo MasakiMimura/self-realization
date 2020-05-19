14b159de3a173bb4865c7cb0739b685f4bd28392
MasakiMimura <mimu36projectd@gmail.com>
2020-05-20 04:07:33 +0900

    function-fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :users, only: [:edit, :update]
-  resources :routines, only: [:create]
+  resources :routines, only: [:create] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
   resources :posts, only: [:index, :create]
   resources :favorites, only: [:new, :create]
   resources :todos, only: [:index, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -51,15 +51,17 @@ ActiveRecord::Schema.define(version: 20200414131907) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
+    t.string   "name",                                              null: false
     t.string   "image"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "twitter",                              default: "", null: false
+    t.text     "content",                limit: 65535
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
