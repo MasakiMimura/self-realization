0414d82da4093c5c5a115225befa5f6baf8a5ff5
MasakiMimura <mimu36projectd@gmail.com>
2020-05-19 10:23:51 +0900

    markup-fix

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,21 +1,21 @@
 $(function() {
   function buildHTML(todo) {
     let html = `
-    <div class="right-content__form__todo__todos__box">
-      <li class="right-content__form__todo__todos__box__list">
-        <div class="right-content__form__todo__todos__box__list__title">
+    <div class="right-content__todo__todos__box">
+      <li class="right-content__todo__todos__box__list">
+        <div class="right-content__todo__todos__box__list__title">
           ${todo.content}
         </div>
-        <span class="right-content__form__todo__todos__box__list__btn--remove js-remove-btn" data-todo-id="${todo.id}">完了</span>
+        <span class="right-content__todo__todos__box__list__btn--remove js-remove-btn" data-todo-id="${todo.id}">完了</span>
       </li>
     </div>
     `;
-    $(".right-content__form__todo__todos").append(html);
+    $(".right-content__todo__todos").append(html);
   }
 
-  $('.right-content__form__todo__form').on('submit', function(e) {
+  $('.right-content__todo__form').on('submit', function(e) {
     e.preventDefault();
-    var todo = $('.right-content__form__todo__form__text').val();
+    var todo = $('.right-content__todo__form__text').val();
     $.ajax({
       type: 'POST',
       url: '/todos.json',
@@ -28,17 +28,17 @@ $(function() {
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.right-content__form__todo__form')[0].reset();
-      $('.right-content__form__todo__form__submit').prop('disabled', false);
-      var result = $('.right-content__form__todo__form__submit').prop('disabled');
+      $('.right-content__todo__form')[0].reset();
+      $('.right-content__todo__form__submit').prop('disabled', false);
+      var result = $('.right-content__todo__form__submit').prop('disabled');
       console.log(result)
     })
     .fail(function() {
       alert('error');
-      $('.right-content__form__todo__form__submit').prop('disabled', false);
+      $('.right-content__todo__form__submit').prop('disabled', false);
     });
   });
-  $(document).on("click", ".right-content__form__todo__todos__box__list__btn--remove", function() {
+  $(document).on("click", ".right-content__todo__todos__box__list__btn--remove", function() {
     const todoId = $(this).attr("data-todo-id");
     console.log(todoId)
     $(this)
